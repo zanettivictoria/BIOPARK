48b97235dadca2564a1e141e1189e0efd07e4232
hw_devs <hw_devs@getbyond.com>
2020-11-12 18:47:57 -0300

    incio banco de dados + com aplicação.

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 ### Commands
 
 `npm install` -- instala todos os pacotes necessários para rodar a aplicação.
+
+### Alterações: 
+
+No arquivo  *#\node_modules\knex\lib\dialects\mysql\schema\columncompiler.js*
+
+Alterar a linha 7 para:
+`const commentEscapeRegex = /(?:<(tag)>)((?:.(?!<\/\1>))+.)(?:<\/\1>)/g // /(?<!\\)'/g;`
+
+No payload de configuração do banco, coloque seu usuário e senha.
---
+++ index.js
@@ -0,0 +1,55 @@
+const restify = require('restify');
+const errs = require('restify-errors');
+
+const server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'root',
+      password : 'Vic0221-',
+      database : 'biopark'
+    }
+  });
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+
+
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
+server.get('/',  (req, res, next) =>  {
+    knex('cadastro').then((dados) => {
+        res.send(dados);
+    }, next)
+
+  });
+
+server.post('/agendamento', (req, res, next) => {
+    knex('cadastro')
+        .insert(req.body)
+        .then((dados) => {
+            res.send(dados);
+        }, next)
+});
+
+server.get('/show/:id',  (req, res, next) =>  {
+    const { id } = req.params;
+    knex('cadastro')
+        .where('id', id)
+        .first() //primeiro
+        .then((dados) => {
+            if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+            
+            res.send(dados);
+    }, next)
+
+  });
