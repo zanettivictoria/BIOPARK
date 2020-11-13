498affcc2b02d7af740331155fc842df2df2be6c
hw_devs <hw_devs@getbyond.com>
2020-11-13 15:44:46 -0300

    alteração no banco de dados, inclusão "messageToSend".

--- CreateDB.sql
+++ CreateDB.sql
@@ -3,15 +3,17 @@ CREATE DATABASE biopark;
 create table cadastro(
 id_age INT NOT NULL auto_increment,
 NameToSend varchar(100) NOT NULL,
-Comunication varchar(100) not NULL,
+ComunicationType varchar(100) not NULL,
+Contact varchar(100) not NULL,
 DateToSend varchar(11) not null,
 MessageSentStatus bool not null, 
+MessageToSend varchar(20000) not NULL,
 primary key(id_age)
 );
 
-insert into cadastro (NameToSend, Comunication, DateToSend,MessageSentStatus) values ("Victoria", "victoria@myEmail.com","2020-12-31", false);
-insert into cadastro (NameToSend, Comunication, DateToSend, MessageSentStatus) values ("Stefani", "Stefani@myEmail.com","2020-12-31", false);
-insert into cadastro (NameToSend, Comunication, DateToSend,MessageSentStatus) values ("Julia", "Julia@myEmail.com","2020-12-31", false);
+insert into cadastro (NameToSend, ComunicationType, Contact, DateToSend,MessageSentStatus, MessageToSend) values ("Victoria", "email","victoria@myEmail.com","2020-12-31", false, "Aqui é a da BIOPARK temos um agendamento!");
+insert into cadastro (NameToSend, ComunicationType, Contact, DateToSend, MessageSentStatus, MessageToSend) values ("Stefani", "email", "Stefani@myEmail.com","2020-12-31", false, "Aqui é a da BIOPARK temos um agendamento!");
+insert into cadastro (NameToSend, ComunicationType, Contact, DateToSend,MessageSentStatus, MessageToSend) values ("Julia", "email", "Julia@myEmail.com","2020-12-31", false, "Aqui é a da BIOPARK temos um agendamento!");
 
 Select *from cadastro;
 # SE PRECISAR ALTERAR USUARIO E SENHA DO BANCO DE DADOS | DESCOMENTAR A LINHA ABAIXO
